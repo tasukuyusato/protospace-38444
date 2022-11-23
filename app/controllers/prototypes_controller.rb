7427af1fa57551a6a6f07067b7f6682349f212b5
yusato <yu.sato0615@gmail.com>
2022-11-23 15:37:28 +0900

    destroyアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -39,12 +39,12 @@ class PrototypesController < ApplicationController
     else
       render :edit
     end
+  end
 
-    def destroy
+  def destroy
       @prototype = Prototype.find(params[:id])
       @prototype.destroy
       redirect_to root_path
-    end
   end
 
   def move_to_index
